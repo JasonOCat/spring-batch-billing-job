9bcc3aa8660dcd0f55b2c32f9325d10cce2c675a
JasonOCat <jason.catbagan@gmail.com>
2025-08-07 14:08:52 +0200

    Add PostgreSQL setup and Docker Compose integration

--- src/main/java/com/jason/springbatchbillingjob/BillingJobConfiguration.java
+++ src/main/java/com/jason/springbatchbillingjob/BillingJobConfiguration.java
@@ -1,4 +1,7 @@
 package com.jason.springbatchbillingjob;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class BillingJobConfiguration {
 }
--- src/main/java/com/jason/springbatchbillingjob/SpringBatchBillingJobApplication.java
+++ src/main/java/com/jason/springbatchbillingjob/SpringBatchBillingJobApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBatchBillingJobApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBatchBillingJobApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchBillingJobApplication.class, args);
+    }
 
 }
