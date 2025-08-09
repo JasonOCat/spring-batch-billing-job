02c7c72260baeae43eed8757fe1f10242e2c82ab
JasonOCat <jason.catbagan@gmail.com>
2025-08-09 07:34:34 +0200

    Refactor billing job to handle file input via parameters
    Updated the BillingJob to dynamically retrieve the input file path from job parameters.

--- src/main/java/com/jason/springbatchbillingjob/BillingJob.java
+++ src/main/java/com/jason/springbatchbillingjob/BillingJob.java
@@ -1,10 +1,11 @@
 package com.jason.springbatchbillingjob;
 
-import org.springframework.batch.core.BatchStatus;
-import org.springframework.batch.core.ExitStatus;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.repository.JobRepository;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class BillingJob implements Job {
 
@@ -21,9 +22,24 @@ public class BillingJob implements Job {
 
     @Override
     public void execute(JobExecution execution) {
-        System.out.println("processing billing information");
+        //using path of parameters directly
+//        String filePath = "./src/main/resources/billing-2025-01.csv";
+//        Path path = Paths.get(filePath);
+//        if (!Files.exists(path)) {
+//            throw new IllegalArgumentException("Le fichier " + filePath + " n'existe pas ou n'est pas lisible.");
+//        }
+//        JobParameters jobParameters = new JobParametersBuilder()
+//                .addString("input.file", "/src/to/file.csv")
+//                .toJobParameters();
+
+        //get job parameters from command line java -jar target/spring-batch-billing-job-0.0.1-SNAPSHOT.jar input.file=src/main/resources/billing-2025-01.csv
+        JobParameters jobParameters = execution.getJobParameters();
+        String inputFile = jobParameters.getString("input.file");
+        System.out.println("processing billing information from file " + inputFile);
         execution.setStatus(BatchStatus.COMPLETED);
         execution.setExitStatus(ExitStatus.COMPLETED);
         this.jobRepository.update(execution);
+
     }
+
 }
